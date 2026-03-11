4ff397dca3323794a37ac47bdd4e8684880defea
Manuel Woelker <manuel.woelker+github@gmail.com>
2026-03-11 21:07:26 +0100

    chore(verification): add new test cases

---
+++ verification/sources/verify/simple/ExprAddStringIntConstant.java
@@ -0,0 +1,7 @@
+package verify.simple;
+
+public class ExprAddStringIntConstant {
+    public static void main( String[] args ) {
+        System.out.println( "Hello, World!"+42 );
+    }
+}
---
+++ verification/unsupported/ExprAddStringInt.java
@@ -0,0 +1,8 @@
+package verify.simple;
+
+public class ExprAddStringInt {
+    public static void main( String[] args ) {
+        int i = 42;
+        System.out.println( "Hello, World!"+i );
+    }
+}
